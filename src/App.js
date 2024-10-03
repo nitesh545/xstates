0dab502588f5d78b0ca77d9d60029da97208375b
nitesh545 <hitman12457868@gmail.com>
2024-10-04 04:03:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState('Select Country');
+  const [selectedCity, setSelectedCity] = useState('Select City');
+  const [selectedState, setSelectedState] = useState('Select State');
+
+  let fetchCountries = () => {
+    fetch("https://crio-location-selector.onrender.com/countries")
+        .then(res => res.json())
+        .then(data => {
+          setCountries([...data, 'Select Country']);
+        })
+        .catch(error => console.log(error));
+  }
+
+  let fetchStates = () => {
+    fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`)
+        .then(res => res.json())
+        .then(data => setStates([...data, 'Select State']))
+        .catch(error => console.log(error));
+  }
+
+  let fetchCities = (countryName, stateName) => {
+    fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`)
+        .then(res => res.json())
+        .then(data => setCities([...data, 'Select City']))
+        .catch(error => console.log(error));
+  }
+
+  useEffect(() => {fetchCountries(); fetchStates(); fetchCities();}, [selectedCountry, selectedState, selectedCity]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Select Location</h1>
+      <select style={{minWidth: '15rem'}} value={selectedCountry} onChange={(event)=>{setSelectedCountry(event.target.value);}}>
+        {countries.map((country, index) => (<option key={index} value={country}>{country}</option>))}
+      </select>
+        <select style={{minWidth: '15rem'}} value={selectedState} onChange={(event)=>{setSelectedState(event.target.value);}}>
+            {states.map((state, index) => (<option key={index} value={state}>{state}</option>))}
+        </select>
+        <select style={{minWidth: '15rem'}} value={selectedCity} onChange={(event)=>{setSelectedCity(event.target.value)}}>
+            {cities.map((city, index) => (<option key={index} value={city}>{city}</option>))}
+        </select>
+      {
+        selectedCity !== 'Select City' && (<h2>You Selected {selectedCity}, {selectedState}, {selectedCountry}</h2>)
+      }
     </div>
   );
 }
