6477c36c58762d6a3482101d28e8ae87807caa0c
nitesh545 <hitman12457868@gmail.com>
2024-10-04 04:31:23 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [states, setStates] = useState([]);
   const [cities, setCities] = useState([]);
-  const [selectedCountry, setSelectedCountry] = useState('Select Country');
-  const [selectedCity, setSelectedCity] = useState('Select City');
-  const [selectedState, setSelectedState] = useState('Select State');
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+  const [selectedState, setSelectedState] = useState('');
 
   let fetchCountries = () => {
     fetch("https://crio-location-selector.onrender.com/countries")
         .then(res => res.json())
         .then(data => {
-          setCountries([...data, 'Select Country']);
+          setCountries(data);
         })
         .catch(error => console.error(error));
   }
@@ -22,14 +22,14 @@ function App() {
   let fetchStates = () => {
     fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`)
         .then(res => res.json())
-        .then(data => setStates([...data, 'Select State']))
+        .then(data => setStates(data))
         .catch(error => console.error(error));
   }
 
   let fetchCities = (countryName, stateName) => {
     fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`)
         .then(res => res.json())
-        .then(data => setCities([...data, 'Select City']))
+        .then(data => setCities(data))
         .catch(error => console.error(error));
   }
 
@@ -38,17 +38,20 @@ function App() {
   return (
     <div className="App">
       <h1>Select Location</h1>
-      <select style={{minWidth: '15rem'}} value={selectedCountry} onChange={(event)=>{setSelectedCountry(event.target.value);}}>
+      <select style={{minWidth: '15rem'}} value={selectedCountry} defaultValue='Select Country' onChange={(event)=>{setSelectedCountry(event.target.value);}}>
+        <option value="Select Country">Select Country</option>
         {countries.map((country, index) => (<option key={index} value={country}>{country}</option>))}
       </select>
-        <select style={{minWidth: '15rem'}} value={selectedState} onChange={(event)=>{setSelectedState(event.target.value);}}>
+        <select style={{minWidth: '15rem'}} value={selectedState} defaultValue='Select State' onChange={(event)=>{setSelectedState(event.target.value);}}>
+          <option value="Select State">Select State</option>
             {states.map((state, index) => (<option key={index} value={state}>{state}</option>))}
         </select>
-        <select style={{minWidth: '15rem'}} value={selectedCity} onChange={(event)=>{setSelectedCity(event.target.value)}}>
+        <select style={{minWidth: '15rem'}} value={selectedCity} defaultValue='Select City' onChange={(event)=>{setSelectedCity(event.target.value)}}>
+          <option value="Select City">Select City</option>
             {cities.map((city, index) => (<option key={index} value={city}>{city}</option>))}
         </select>
       {
-        selectedCity !== 'Select City' && (<h2>You selected {selectedCity}, {selectedState}, {selectedCountry}</h2>)
+        selectedCity !== '' && (<h2>You selected {selectedCity}, {selectedState}, {selectedCountry}</h2>)
       }
     </div>
   );
